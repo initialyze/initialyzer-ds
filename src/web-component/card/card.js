16becff0706b6850a702096584b33bcc7747709a
varshish <varshish@initialyze.com>
2021-01-28 21:20:08 +0530

    card web component changes

--- src/web-component/card/card.js
+++ src/web-component/card/card.js
@@ -5,8 +5,8 @@ class Card extends LitElement {
   static get properties() {
     return {
       assetVariation: { type: String },
-      imageResource: {},
-      preTitle: { tyoe: String },
+      imageResource: { type:Object},
+      pretitle: { type: String },
       title: { type: String },
       titleType: { type: String },
       description: { type: String },
@@ -15,10 +15,9 @@ class Card extends LitElement {
       titleLinkHidden: { type: String },
       linkURL: { type: String },
       actionsEnabled: { type: String },
-
       template: { type: String },
       linkEntireCard: {},
-      actionsList: {}
+      actionsList: {type : Object}
     };
   }
 
@@ -26,15 +25,38 @@ class Card extends LitElement {
     super();
     this.assetVariation = 'Default';
     this.title = 'Card Title';
+    this.linkURL = 'https://publish-aem65-int.initialyze.com/';
+    this.titleType = 'h1';
     this.description = 'Add some description here...';
     this.variation = 'Default';
+    this.pretitle = "News";
     this.linkEntireCard = 'false';
     this.titleLinkHidden = 'true';
     this.actionsEnabled = 'true';
   }
 
   render() {
-    console.log("Initializing card component ...");
+    console.log("Initializing card component ...",this.actionsList);
+    let customElement;
+    // IF LINK DISABLED
+    if (this.variation == "Default") {
+      customElement = `
+      <div class="cmp-card__content card-body">
+        <div class="cmp-card__content-wrapper">
+          <h6><span class="cmp-card__eyebrow">${this.pretitle}</span></h6>  
+          <h2 class="cmp-card__card-title" data-sly-element="${this.titleType}">
+            <a class="cmp-card__title-link" href="${this.linkURL}" data-sly-unwrap="${this.linkEntireCard || !this.linkURL || this.titleLinkHidden}">${this.title}</a>
+          </h2>
+          <div class="cmp-card__description">${this.description}</div>
+          <div class="cmp-card__action-container" data-sly-test="${this.actionsEnabled}" data-sly-list.button="${this.actionsList}">
+            <div class="cmp-card__action-item" data-sly-resource="${this.actionsList[button]}"></div>
+          </div>
+        </div>
+      </div>`;
+    } 
+    return html`
+      ${unsafeHTML(customElement)}
+    `;
   }
 }
 
